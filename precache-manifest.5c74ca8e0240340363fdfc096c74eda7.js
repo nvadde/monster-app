5cbb0e9a5d4f5a47a67f0a421b5dd0584d0a973d
Nathaniel <vadde.nathaniel@gmail.com>
2020-06-27 16:05:14 +0530

    Updates

---
+++ precache-manifest.5c74ca8e0240340363fdfc096c74eda7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd31dba62285306b64b05c3d1e64612e",
+    "url": "/monster-app/index.html"
+  },
+  {
+    "revision": "7b08185e05bd0adb3da1",
+    "url": "/monster-app/static/css/main.1c496f75.chunk.css"
+  },
+  {
+    "revision": "6952c745d55c5a0fa7de",
+    "url": "/monster-app/static/js/2.e47ca8d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-app/static/js/2.e47ca8d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b08185e05bd0adb3da1",
+    "url": "/monster-app/static/js/main.ad7b55f5.chunk.js"
+  },
+  {
+    "revision": "70ffa8869a950765e1c3",
+    "url": "/monster-app/static/js/runtime-main.4edd5f74.js"
+  }
+]);
